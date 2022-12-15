e64371e908ce698bddd9e8777e2ba9a7eb773c56
Luciano Pena <lucianonp23@gmail.com>
2022-12-15 13:25:50 +0000

    logical and style done, working on deploy

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,27 +4,29 @@ import AddToList from "./AddToList";
 const MovieList = (props) => {
     
     
-    return ( 
-        <div className="row_posters">
-           {props.movies.map((item, index)=> {
-            return(
-                <div className="row_poster_container"> 
-                    <img src={item.Poster} alt="Poster" className="row_poster"></img>
-                    <div className="image_overlay">
-                        <h2>{item.Title}</h2>
-						<p> <button className="manageButton" onClick={()=> props.manageItem(item)}>{props.favoriteComponent} </button></p>
-                        
-                    </div>
-                
-                </div>
-                    
-                
-                
-            )}
-           )} 
-            
-        </div>
-     );
+    return (
+      <div className="row_posters">
+        {props.movies.map((item, index) => {
+          return (
+            <div className="row_poster_container">
+              <img src={item.Poster} alt="Poster" className="row_poster"></img>
+              <div className="image_overlay">
+                <h2>{item.Title}</h2>
+                <p>
+                  {" "}
+                  <button
+                    className="manageButton"
+                    onClick={() => props.manageItem(item)}
+                  >
+                    {props.favoriteComponent}{" "}
+                  </button>
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
 }
  
 export default MovieList;
--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,8 +1,11 @@
 const SearchBox = (props) => {
-    return ( 
-        <input   className="search_place" placeholder="Type your search ..." onChange={(e)=> props.setSearchValue(e.target.value)} />
-                    
-     );
+  return (
+    <input
+      className="search_place"
+      placeholder="Type your search ..."
+      onChange={(e) => props.setSearchValue(e.target.value)}
+    />
+  );
 }
  
 export default SearchBox;
